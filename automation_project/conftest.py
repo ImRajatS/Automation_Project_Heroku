c47d041c9ee675dc8b2bb9b16d4431a62778c75f
ImRajatS <amrajat70@gmail.com>
2026-05-05 13:19:03 +0530

    Added conftest browser launch code

---
+++ automation_project/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+import os
+
+
+@pytest.fixture(scope="class")
+def init_driver(request):
+
+    supported_browser = ['chrome', 'ch', 'firefox', 'ff']
+
+    browser = os.environ.get('BROWSER', None)
+
+    if not browser:
+        raise Exception("The environment variable 'BROWSER' must be set")
+
+    browser = browser.lower()
+    if browser not in supported_browser:
+        raise Exception(f"Provided browser {browser}, supported are {supported_browser}")
+
+    if browser in ('chrome', 'ch'):
+        driver = webdriver.Chrome()
+    elif browser in ('firefox', 'ff'):
+        driver = webdriver.Firefox()
+
+
+    request.cls.driver = driver
+    yield
+    driver.quit()
